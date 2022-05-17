734e9fe4689b4ec676f71f16b1481adfafaeec37
hanyu1981 <cbest@mue.biglobe.ne.jp>
2022-05-17 10:57:17 +0900

    update registrationController

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class RegistrationController extends Controller
 {
-    //
+    public function Start(Request $request)
+    {
+    	$response = array();
+    	
+    	return $response;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('registration','RegistrationController@Start');
